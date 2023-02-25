de90140d19228934e8a0bdf9f1e6ef1387eaaf74
jycforest29 <jycforest29.dev@gmail.com>
2023-02-25 13:06:08 +0900

    debug: AuthenticationManager 빈 등록 안되는 문제 해결

--- src/main/java/com/jycforest29/commerce/security/config/WebSecurityConfig.java
+++ src/main/java/com/jycforest29/commerce/security/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +29,11 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 //    public WebSecurityCustomizer webSecurityCustomizer(){
 //        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 //    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
