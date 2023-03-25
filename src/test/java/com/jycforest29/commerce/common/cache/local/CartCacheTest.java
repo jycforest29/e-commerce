c4f510d9e17be917a67afb371064bbe8e17356d0
jycforest29 <jycforest29.dev@gmail.com>
2023-03-25 18:54:16 +0900

    test: Cart 패키지의 @CachePut, @CacheEvict 테스트 추가

--- src/test/java/com/jycforest29/commerce/common/cache/local/CartCacheTest.java
+++ src/test/java/com/jycforest29/commerce/common/cache/local/CartCacheTest.java
@@ -1,5 +1,6 @@
 package com.jycforest29.commerce.common.cache.local;
 
+import com.jycforest29.commerce.cart.domain.entity.CartUnit;
 import com.jycforest29.commerce.cart.domain.repository.CartUnitRepository;
 import com.jycforest29.commerce.cart.service.CartServiceImpl;
 import com.jycforest29.commerce.item.domain.entity.Item;
@@ -13,15 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.atMostOnce;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class CartCacheTest extends DockerComposeTestContainer {
@@ -33,8 +31,6 @@ public class CartCacheTest extends DockerComposeTestContainer {
     private CartUnitRepository cartUnitRepository;
     @Autowired
     private CartServiceImpl cartService;
-    @PersistenceContext
-    private EntityManager entityManager;
 
     AuthUser authUser = AuthUser.builder()
             .username("test_username")
@@ -69,20 +65,47 @@ public class CartCacheTest extends DockerComposeTestContainer {
                     .willReturn(Optional.ofNullable(authUser));
             given(itemRepository.findById(1L))
                     .willReturn(Optional.ofNullable(item));
-            //when, then
+            //when
             cartService.addCartUnitToCart(1L, 1, authUser.getUsername());
-            //then
-            assertThat(cartService.getCartUnitList(authUser.getUsername())).isEqualTo(1);
+            //then - getCartUnitList은 @Cacheable로 조회함
+            assertThat(cartService.getCartUnitList(authUser.getUsername()).getCartUnitResponseDtoList().size())
+                    .isEqualTo(1);
+            verify(authUserRepository, atMostOnce()).findByUsername(authUser.getUsername());
         }
 
         @Test
         void CartUnit을_삭제하여_CachePut_어노테이션을_테스트한다(){
-            //given
-            given(itemRepository.findById(1L))
-                    .willReturn(Optional.ofNullable(item));
-            //when
-            cartService.addCartUnitToCart(1L, 1, authUser.getUsername());
+            //given - 장바구니에 item(가격 1000원, 수량 1개), otherItem(가격 500원, 수량 1개) 추가함
+            Item otherItem = Item.builder()
+                    .name("other_item")
+                    .price(500)
+                    .number(1)
+                    .build();
+
+            CartUnit cartUnit = CartUnit.builder()
+                    .item(item)
+                    .number(1)
+                    .build();
+            authUser.getCart().addCartUnitToCart(cartUnit, item.getPrice());
+            cartUnitRepository.saveAndFlush(cartUnit);
+
+            CartUnit otherCartUnit = CartUnit.builder()
+                    .item(otherItem)
+                    .number(1)
+                    .build();
+            authUser.getCart().addCartUnitToCart(otherCartUnit, otherItem.getPrice());
+            cartUnitRepository.saveAndFlush(otherCartUnit);
+
+            given(authUserRepository.findByUsername(authUser.getUsername()))
+                    .willReturn(Optional.ofNullable(authUser));
+            given(cartUnitRepository.findById(1L))
+                    .willReturn(Optional.ofNullable(cartUnit));
+            //when - item 제거
+            cartService.deleteCartUnit(1L, authUser.getUsername());
             //then
+            assertThat(cartService.getCartUnitList(authUser.getUsername()).getCartUnitResponseDtoList().size())
+                    .isEqualTo(1);
+            assertThat(cartService.getCartUnitList(authUser.getUsername()).getTotalPrice()).isEqualTo(500);
             verify(authUserRepository, atMostOnce()).findByUsername(authUser.getUsername());
         }
 
@@ -92,11 +115,15 @@ public class CartCacheTest extends DockerComposeTestContainer {
             //given
             given(authUserRepository.findByUsername(authUser.getUsername()))
                     .willReturn(Optional.ofNullable(authUser));
+            given(itemRepository.findById(1L))
+                    .willReturn(Optional.ofNullable(item));
             //when
-            IntStream.range(0, 10)
-                    .forEach(i -> cartService.getCartUnitList(authUser.getUsername()));
+            cartService.addCartUnitToCart(1L, 1, authUser.getUsername());
+            cartService.deleteCartAll(authUser.getUsername());
             //then
-            verify(authUserRepository, atMostOnce()).findByUsername(authUser.getUsername());
+            assertThat(cartService.getCartUnitList(authUser.getUsername()).getCartUnitResponseDtoList().size())
+                    .isEqualTo(0);
+            verify(authUserRepository, times(3)).findByUsername(authUser.getUsername());
         }
     }
 }
