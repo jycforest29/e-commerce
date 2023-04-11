11676631a8916f5d5b612ce1d752cdb1ced15b71
jycforest29 <jycforest29.dev@gmail.com>
2023-04-11 13:24:40 +0900

    refactor: batch bean 오류로 batch 모듈 제외

--- src/main/java/com/jycforest29/commerce/common/config/batch/BatchConfig.java
+++ src/main/java/com/jycforest29/commerce/common/config/batch/BatchConfig.java
@@ -1,78 +1,78 @@
-package com.jycforest29.commerce.common.config.batch;
-
-import com.jycforest29.commerce.order.domain.entity.MadeOrder;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
-import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManagerFactory;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class BatchConfig {
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-    @Bean
-    public JpaPagingItemReader<MadeOrder> itemReader(){
-        LocalDateTime now = LocalDateTime.now();
-        Map<String, LocalDateTime> map = new HashMap<>();
-        map.put("now", now);
-        return new JpaPagingItemReaderBuilder<MadeOrder>()
-                .pageSize(10)
-                .queryString("select m from MadeOrder m where timestampdiff(day, m.createdAt, :now) = 5")
-                .entityManagerFactory(entityManagerFactory)
-                .build();
-    }
-
-    @Bean
-    public ItemProcessor<MadeOrder, MadeOrder> itemProcessor(){
-        return new ItemProcessor<MadeOrder, MadeOrder>() {
-            @Override
-            public MadeOrder process(MadeOrder madeOrder) throws Exception {
-                if (madeOrder.getCancelAvaiable()){
-                    madeOrder.setCancelAvaiable(false);
-                }
-                return madeOrder;
-            }
-        };
-    }
-
-    @Bean
-    public ItemWriter itemWriter(){
-        return new JpaItemWriterBuilder<MadeOrder>()
-                .entityManagerFactory(entityManagerFactory)
-                .build();
-    }
-
-    @Bean
-    public Job job(){
-        return jobBuilderFactory.get("job")
-                .start(step())
-                .build();
-    }
-
-    @Bean
-    public Step step(){
-        return stepBuilderFactory.get("step1")
-                .<MadeOrder, MadeOrder>chunk(10)
-                .reader(itemReader())
-                .processor(itemProcessor())
-                .writer(itemWriter())
-                .build();
-    }
-}
+//package com.jycforest29.commerce.common.config.batch;
+//
+//import com.jycforest29.commerce.order.domain.entity.MadeOrder;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.item.ItemProcessor;
+//import org.springframework.batch.item.ItemWriter;
+//import org.springframework.batch.item.database.JpaPagingItemReader;
+//import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+//import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.persistence.EntityManagerFactory;
+//import java.time.LocalDateTime;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class BatchConfig {
+//    @Autowired
+//    private JobBuilderFactory jobBuilderFactory;
+//    @Autowired
+//    private StepBuilderFactory stepBuilderFactory;
+//    @Autowired
+//    private EntityManagerFactory entityManagerFactory;
+//    @Bean
+//    public JpaPagingItemReader<MadeOrder> itemReader(){
+//        LocalDateTime now = LocalDateTime.now();
+//        Map<String, LocalDateTime> map = new HashMap<>();
+//        map.put("now", now);
+//        return new JpaPagingItemReaderBuilder<MadeOrder>()
+//                .pageSize(10)
+//                .queryString("select m from MadeOrder m where timestampdiff(day, m.createdAt, :now) = 5")
+//                .entityManagerFactory(entityManagerFactory)
+//                .build();
+//    }
+//
+//    @Bean
+//    public ItemProcessor<MadeOrder, MadeOrder> itemProcessor(){
+//        return new ItemProcessor<MadeOrder, MadeOrder>() {
+//            @Override
+//            public MadeOrder process(MadeOrder madeOrder) throws Exception {
+//                if (madeOrder.getCancelAvaiable()){
+//                    madeOrder.setCancelAvaiable(false);
+//                }
+//                return madeOrder;
+//            }
+//        };
+//    }
+//
+//    @Bean
+//    public ItemWriter itemWriter(){
+//        return new JpaItemWriterBuilder<MadeOrder>()
+//                .entityManagerFactory(entityManagerFactory)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Job job(){
+//        return jobBuilderFactory.get("batch-job")
+//                .start(step())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step(){
+//        return stepBuilderFactory.get("step1")
+//                .<MadeOrder, MadeOrder>chunk(10)
+//                .reader(itemReader())
+//                .processor(itemProcessor())
+//                .writer(itemWriter())
+//                .build();
+//    }
+//}
--- src/main/java/com/jycforest29/commerce/common/config/batch/SchedulerConfig.java
+++ src/main/java/com/jycforest29/commerce/common/config/batch/SchedulerConfig.java
@@ -1,35 +1,33 @@
-package com.jycforest29.commerce.common.config.batch;
-
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class SchedulerConfig {
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Autowired
-    private BatchConfig batchConfig;
-
-    @Scheduled(cron = "0 0 24 * * *")
-    public void runJob() throws JobInstanceAlreadyCompleteException,
-            JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-        Map<String, JobParameter> jobParameterMap = new HashMap<>();
-        jobParameterMap.put("time", new JobParameter(System.currentTimeMillis()));
-        JobParameters jobParameters = new JobParameters(jobParameterMap);
-        jobLauncher.run(batchConfig.job(), jobParameters);
-    }
-
-}
+//package com.jycforest29.commerce.common.config.batch;
+//
+//import org.springframework.batch.core.JobParameter;
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.JobParametersInvalidException;
+//import org.springframework.batch.core.launch.JobLauncher;
+//import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+//import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+//import org.springframework.batch.core.repository.JobRestartException;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.Scheduled;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class SchedulerConfig {
+//    @Autowired
+//    private JobLauncher jobLauncher;
+//
+//    @Autowired
+//    private BatchConfig batchConfig;
+//
+//    @Scheduled(cron = "0 0 24 * * *")
+//    public void runJob() throws JobInstanceAlreadyCompleteException,
+//            JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+//        Map<String, JobParameter> jobParameterMap = new HashMap<>();
+//        jobParameterMap.put("time", new JobParameter(System.currentTimeMillis()));
+//        JobParameters jobParameters = new JobParameters(jobParameterMap);
+//        jobLauncher.run(batchConfig.job(), jobParameters);
+//    }
+//}
